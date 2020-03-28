6b408a3dee03df5ff025bd1af807ee045f2a25f1
Tomasz Kolnierzak <t.kolnierzak@gmail.com>
2020-03-28 18:44:56 +0100

    Dodaj pliki projektów.

---
+++ cw2/Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw2.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        public string GetStudent()
+        {
+            return "Kowalski, Malewski, Andrzejewski";
+        }
+    }
+}
