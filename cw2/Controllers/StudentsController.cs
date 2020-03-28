fb34113c45f635d366ff7ba78f36179097b12308
Tomasz Kolnierzak <t.kolnierzak@gmail.com>
2020-03-28 19:01:59 +0100

    Zadanie 4

--- cw2/Controllers/StudentsController.cs
+++ cw2/Controllers/StudentsController.cs
@@ -14,5 +14,20 @@ namespace cw2.Controllers
         {
             return "Kowalski, Malewski, Andrzejewski";
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono studenta");
+        }
     }
+
 }
